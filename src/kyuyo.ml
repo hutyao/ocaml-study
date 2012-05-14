f4df8eb3ce191c6342afe0ba06d755a1ceecc9c4
hutyao <hutyao@gmail.com>
2012-05-14 22:35:51 +0900

    add description

--- src/kyuyo.ml
+++ src/kyuyo.ml
@@ -1,3 +1,6 @@
+(* 優遇時給（円）*)
+let yugu_jikyu = 980
+
 (* 時給（円） *)
 let jikyu = 950
 
@@ -6,9 +9,10 @@ let kihonkyu = 100
 
 (* 目的：働いた時間 x に応じたアルバイト代を計算する *)
 (* kyuyo：int -> int *)
-let kyuyo x = kihonkyu + x * jikyu
+let kyuyo x =
+	kihonkyu + x * (if x < 30 then jikyu else yugu_jikyu)
 
 (* テスト *)
 let test1 = kyuyo 25 = 23850
 let test2 = kyuyo 28 = 26700
-let test3 = kyuyo 31 = 29550
+let test3 = kyuyo 31 = 30480
